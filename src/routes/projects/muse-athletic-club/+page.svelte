99067c51b1f008782a06ebced5dd0473d37b41f4
Tim Wood <washwithcare@gmail.com>
2024-01-06 20:18:42 -0600

    Athletic club placeholders

--- src/routes/projects/muse-athletic-club/+page.svelte
+++ src/routes/projects/muse-athletic-club/+page.svelte
@@ -1,2 +1,52 @@
 <script lang="ts">
 </script>
+
+Intro Image 1280x1280
+<br />
+
+Full Bleed photo 3000x2000
+<br />
+
+Site plan 1600x1600
+<br />
+
+Day use Diagram 1600w
+<br />
+
+Views diagram 1600w
+<br />
+
+Program diagram 1600w
+<br />
+
+Full bleed section 3000x2000
+<br />
+
+Section 1600w
+<br />
+
+Plans 1600w
+<br />
+
+Ground plan 3000x2000
+<br />
+
+Pair 1 1280x1280
+<br />
+
+Plan 1 2560w
+<br />
+
+Pair 2 1280x1280
+<br />
+
+Plan 2 2560w
+<br />
+
+Pair 3 1280x1280
+<br />
+
+Plan 3 2560w
+<br />
+
+Full Bleed section photo 3000x2000
