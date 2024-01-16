c6c125524cf45196dd7e519b0329c7bd44d7340a
Tim Wood <washwithcare@gmail.com>
2024-01-16 11:12:24 -0600

    more responsive

--- src/lib/projects/Intro.svelte
+++ src/lib/projects/Intro.svelte
@@ -4,8 +4,8 @@
   export let intro: Intro;
 </script>
 
-<div class="flex items-center py-24 px-3 gap-24">
-  <div class="w-1/2">
+<div class="flex flex-col lg:flex-row items-center py-12 lg:py-24 px-3 gap-12 lg:gap-24">
+  <div class="lg:w-1/2">
     <h2 class="font-bold text-3xl pb-4">{intro.title}</h2>
     {#each intro.subtitle as subtitle}
       <h3 class="font-serif italic">{subtitle}</h3>
@@ -14,7 +14,7 @@
       <p class="font-serif pt-4 text-pretty max-w-prose">{@html description}</p>
     {/each}
   </div>
-  <div class="w-1/2">
+  <div class="lg:w-1/2">
     <img src={intro.image} alt="Intro" />
   </div>
 </div>
--- src/lib/projects/SplitView.svelte
+++ src/lib/projects/SplitView.svelte
@@ -3,26 +3,26 @@
   export let spacing: '1|1' | '1|2' | '2|1' = '1|1';
 
   const leftSpacingMap = {
-    '1|1': 'w-1/2',
-    '1|2': 'w-1/3',
-    '2|1': 'w-2/3'
+    '1|1': 'lg:w-1/2',
+    '1|2': 'lg:w-1/3',
+    '2|1': 'lg:w-2/3'
   };
 
   const rightSpacingMap = {
-    '1|1': 'w-1/2',
-    '1|2': 'w-2/3',
-    '2|1': 'w-1/3'
+    '1|1': 'lg:w-1/2',
+    '1|2': 'lg:w-2/3',
+    '2|1': 'lg:w-1/3'
   };
 
   const alignMap = {
-    top: 'items-start',
-    center: 'items-center',
-    bottom: 'items-end'
+    top: 'lg:items-start',
+    center: 'lg:items-center',
+    bottom: 'lg:items-end'
   };
 </script>
 
-<div class="px-3 my-24">
-  <div class="flex gap-24 {alignMap[align]}">
+<div class="px-3 my-12 my-18 lg:my-24">
+  <div class="flex flex-col lg:flex-row gap-12 lg:gap-24 {alignMap[align]}">
     <div class={leftSpacingMap[spacing]}>
       <slot name="left" />
     </div>
