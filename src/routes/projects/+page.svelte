1e63f36b5e5f8fa3880e6764e330d8909e1779a0
Tim Wood <washwithcare@gmail.com>
2024-01-20 13:53:14 -0600

    Consistent cover text heights

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -13,8 +13,10 @@
         alt="Full Bleed"
         class="w-full h-full object-cover rounded-lg group-hover:opacity-70"
       />
-      <h2 class="absolute bottom-3 inset-x-3 py-2 rounded-b-lg backdrop-blur-sm bg-white/40">
-        <div class="uppercase font-bold text-slate-800 text-center">{project.title}</div>
+      <h2
+        class="absolute bottom-3 inset-x-3 flex justify-center items-center h-14 px-4 rounded-b-lg backdrop-blur-sm bg-white/50"
+      >
+        <div class="uppercase font-bold text-slate-800 leading-5">{project.title}</div>
       </h2>
     </a>
   {/each}
