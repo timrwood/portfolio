30d254bf9833d6ee64668e0347d0c8cde9b91499
Tim Wood <washwithcare@gmail.com>
2024-01-20 15:06:14 -0600

    Fix fixed attachment on ios

--- src/routes/projects/put-away-house/FullBleedScroll.svelte
+++ src/routes/projects/put-away-house/FullBleedScroll.svelte
@@ -7,7 +7,7 @@
     m-[0_-50vw] w-[100vw] relative left-1/2 right-1/2 h-[70vh]
     border-b-2 border-[#35523A]
     bg-repeat-x bg-center bg-fixed
-    bg-[length:130%] lg:bg-[length:110%] min-[2000px]:bg-[length:2500px]
+    bg-[length:130%_auto] lg:bg-[length:110%_auto] min-[2000px]:bg-[length:2500px_auto]
   "
   style="background-image:url({image})"
 ></div>
