fa5e66e17e3b50e62d82f7defa82f133f13e00bc
Tim Wood <washwithcare@gmail.com>
2024-03-09 14:33:00 -0600

    Add a link to the portfolio pdf from the homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,6 +24,22 @@
     <h2 class="text-md sm:text-2xl tracking-widest font-serif italic pt-2">
       Architecture Portfolio
     </h2>
-    <a class="text-sm sm:text-xl tracking-widest pt-8 underline" href="/projects">view projects</a>
+    <a class="text-sm sm:text-xl tracking-widest pt-8 underline" href="/projects"
+      >View projects online</a
+    >
+    <a class="text-sm sm:text-xl tracking-widest pt-4" href="/portfolio">
+      <svg
+        class="inline"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="24"
+        height="24"
+      >
+        <path
+          d="m14 2 6 6v12a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V4c0-1.1.9-2 2-2h8Zm4 18V9h-5V4H6v16h12Zm-2-5-4 4-4-4 1.4-1.4 1.6 1.6V11h2v4.2l1.6-1.6L16 15Z"
+        />
+      </svg>
+      <span class="underline">Download portfolio as PDF</span>
+    </a>
   </div>
 </div>
