8552cd7faf7c84ee61f9fd73a8ac35a3e31211c2
Tim Wood <washwithcare@gmail.com>
2025-01-09 19:36:54 -0600

    Fix description

--- src/routes/projects/play-frame/project.ts
+++ src/routes/projects/play-frame/project.ts
@@ -6,8 +6,11 @@ export default {
   title: 'Play Frame',
   coverImage,
   introImage,
-  subtitle: ['Designed and constructed independently', 'Fall 2024'],
+  subtitle: ['Personal project', 'Fall 2024'],
   description: [
-    'Overlooked by most, the south wall of the Manhattan Building is largely covered by a battered brick wall, the last remaining remnant of a building demolished to make way for Congress Parkway in 1949. This project carefully positions a chiseled, prismatic opaque glass volume set in contrast to the historic context, the ground plane, the rough broken brick and the chaos of the surrounding intersection. Upon entry, guests turn off their devices, meander to and sit in a silent felt-lined room, looking onto this place, contemplating ruin.'
+    'After six years of architecture school, working on assignments during nights and weekends, I promised my kids I would build them a tree house after graduation to make up for the lost time together. After surveying the backyard and not feeling confortable with any of the trees, we decided to build an a-frame playhouse instead.',
+    'The structure is composed of a floating deck on concrete piers, with a pressure-treated half-lap-joined timber frame supporting a semi-transparent polycarbonate roof membrane. Stone slabs will be installed as a transition step between the deck and the grass next spring when the weather warms up.',
+    'The project was also an opportunity to round out my education with some practical experience in project management, budgeting, permitting, and construction. I built the project as I did with other academic projects, over nights and weekends, but this time with the interspersed help of my wife, kids, brother and father.',
+    "I couldn't have chosen a better client for my first architectural project than my two children."
   ]
 } as Project;
