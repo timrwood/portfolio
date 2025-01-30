797240a8f4403412340de8a41f7c388cda10efbd
Tim Wood <washwithcare@gmail.com>
2025-01-29 20:15:33 -0600

    Update pdf link

--- src/routes/portfolio/+page.server.ts
+++ src/routes/portfolio/+page.server.ts
@@ -3,6 +3,6 @@ import { redirect } from '@sveltejs/kit';
 export function load() {
   redirect(
     307,
-    'https://github.com/timrwood/portfolio/releases/download/2024-03-09/Tim.Wood.Portfolio.pdf'
+    'https://github.com/timrwood/portfolio/releases/download/2025-01-29/Tim-Wood-Portfolio-2025.pdf'
   );
 }
